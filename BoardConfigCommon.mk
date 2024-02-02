c82c0495784374a1a5e5bafb5718dada12013431
Luofan Chen <clfbbn@gmail.com>
2024-02-02 20:49:44 +0800

    sm8350-common: Import QCC system_ext blobs
    Makes the QTI GNSS service happy. Thanks luca020400 for the help.
    
    Change-Id: I6de25b0b3fde6cce3a6cdf95f31c68734d429956

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,7 +84,11 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+<<<<<<< HEAD
     vendor/derp/config/device_framework_matrix.xml
+=======
+    vendor/everest/config/device_framework_matrix.xml
+>>>>>>> dbcc045 (sm8350-common: Import QCC system_ext blobs)
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
